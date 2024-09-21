19a8f13f13145d1fe0be1abc6ab42eb6da226b98
Roberto <robertomeneses12513321@gmail.com>
2024-09-21 20:46:20 -0300

    passando o deadlock_manage como parametro

--- transaction_manager.py
+++ transaction_manager.py
@@ -14,21 +14,21 @@ class TransactionManager:
         print(f"Processing {operation} on {resource} by {transaction_id}")
         # Adquirir o lock necessário
         if operation == 'R':
-            return lock_manager.acquire_shared_lock(transaction_id, resource)
+            return lock_manager.acquire_shared_lock(transaction_id, resource, deadlock_manager)
         elif operation == 'W':
-            return lock_manager.acquire_exclusive_lock(transaction_id, resource)
+            return lock_manager.acquire_exclusive_lock(transaction_id, resource, deadlock_manager)
         elif operation == 'U':
-            return lock_manager.acquire_update_lock(transaction_id, resource)
+            return lock_manager.acquire_update_lock(transaction_id, resource, deadlock_manager)
         elif operation == 'C':
-            return lock_manager.acquire_certify_lock(transaction_id, resource)
+            return lock_manager.acquire_certify_lock(transaction_id, resource, deadlock_manager)
         elif operation == 'IR':
-            return lock_manager.acquire_intent_read_lock(transaction_id, resource)
+            return lock_manager.acquire_intent_read_lock(transaction_id, resource, deadlock_manager)
         elif operation == 'IW':
-            return lock_manager.acquire_intent_write_lock(transaction_id, resource)
+            return lock_manager.acquire_intent_write_lock(transaction_id, resource, deadlock_manager)
         elif operation == 'IU':
-            return lock_manager.acquire_intent_update_lock(transaction_id, resource)
+            return lock_manager.acquire_intent_update_lock(transaction_id, resource, deadlock_manager)
         elif operation == 'IC':
-            return lock_manager.acquire_intent_certify_lock(transaction_id, resource)
+            return lock_manager.acquire_intent_certify_lock(transaction_id, resource, deadlock_manager)
 
     def commit_transaction(self, transaction_id):
         print(f"Committing transaction {transaction_id}")
