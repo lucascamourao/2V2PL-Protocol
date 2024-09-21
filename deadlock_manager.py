a275fe1c8aa306c1c4fef685e889fac6f0471e27
Mourvim <108086840+JoaoLucasRodriguesdaSilva@users.noreply.github.com>
2024-09-21 20:08:32 -0300

    renamed:    deadlock_detection.py -> deadlock_manager.py 	modified:   main.py 	modified:   transaction_manager.py

--- deadlock_manager.py
+++ deadlock_manager.py
@@ -1,10 +1,11 @@
 # Funções para detecção de deadlocks (grafo de espera) 
 
 class DeadlockDetection:
-    def __init__(self, schedule):
+    def __init__(self):
         self.wait_grafo = {} #'T1': ['T2', 'T3', 'T4'] == T1->T2, T1->T3, T1->T4
         self.past = []
 
+    def start_grafo(self, schedule):
         for transaction in schedule:
             self.wait_grafo[transaction[0]] = []
 
@@ -24,5 +25,11 @@ class DeadlockDetection:
             if self.detect_deadlock(i, self.wait_grafo[i], past):
                 return True
         return False
+    
+    def recent_transaction(self):
+        for transaction in self.wait_grafo.keys():
+            if transaction in self.past:
+                recent = transaction
+        return recent
     
 # Se existir, retornar a transação a ser abortada
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from transaction_manager import TransactionManager
 from lock_manager import LockManager
-from deadlock_detection import detect_deadlock
+from deadlock_manager import DeadlockDetection
 
 # Iniciar o gerenciador de transações e locks
 transaction_manager = TransactionManager()
 lock_manager = LockManager()
+deadlock_manager = DeadlockDetection()
 
 def main():
     # Exemplo de escalonamento: T1: R(A), W(A), T2: R(B), W(B), ...
@@ -17,11 +18,12 @@ def main():
         ['T3', 'W', 'B']
     ]
 
+    deadlock_manager.start_grafo(schedule)
+
     # Processar o escalonamento
-    for transaction, operation, resource in schedule:
-        if not transaction_manager.process(transaction, operation, resource, lock_manager):
+    for [transaction, operation, resource] in schedule:
+        if not transaction_manager.process(transaction, operation, resource, lock_manager, deadlock_manager):
             print(f"Deadlock detected during {transaction}'s operation on {resource}.")
-            detect_deadlock()
             break
 
 if __name__ == '__main__':
