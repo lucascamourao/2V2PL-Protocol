81c2b75e8de9cff9b5dde0c8d9ce43d6bffa3bb8
lucascamourao <lucascamourao@gmail.com>
2024-09-22 14:06:26 -0300

    usando lista em utils

--- utils.py
+++ utils.py
@@ -1,20 +1,22 @@
 # Funções utilitárias
 
+
 # supondo que cada transação tem 3 operações
 def parse_schedule(input_str):
     parsed_schedule = []
 
     for element in range(0, len(input_str), 3):
-        tuple = (input_str[element], input_str[element+1], input_str[element+2])
+        line = [input_str[element], input_str[element + 1], input_str[element + 2]]
 
-        parse_schedule.append(tuple)
+        parse_schedule.append(line)
 
     # Função para converter uma string de entrada de transações em uma lista estruturada
     # Exemplo de input: "T1: R(A), W(A); T2: R(B), W(B)"
     # Saída: [('T1', 'R', 'A'), ('T1', 'W', 'A'), ('T2', 'R', 'B'), ('T2', 'W', 'B')]
     return parsed_schedule
 
-'''
+
+"""
 Seja S uma string, 
 
 S[0] é a transação. Ex: 'T1'
@@ -23,4 +25,4 @@ S[1] é a operação. Ex: 'R'.
 
 S[2] é o recurso (onde será aplicada a operação). Ex: 'A'.
 
-'''
+"""
